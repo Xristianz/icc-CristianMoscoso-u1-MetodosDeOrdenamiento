e9186193e6dcc0c52e79c468c587b4c209d80348
Cristian_Moscoso <moscosocristhian130@gmail.com>
2025-03-31 16:16:52 -0500

    Update MetodoInsercion.java

--- src/MetodoInsercion.java
+++ src/MetodoInsercion.java
@@ -1,71 +1,78 @@
 public class MetodoInsercion {
-    public int ordenarAsd(int[] arreglo, boolean pasos) {
-        int tam = arreglo.length;
+    public int[] ordenarAscendente(int[] arreglo, boolean pasos) {
+        int n = arreglo.length;
+        int pos = 0;
+        int aux = 0;
         int contComparaciones = 0;
-        
-        for (int i = 1; i < tam; i++) {
-            int aux = arreglo[i];
-            int j = i - 1;
-            
-            if (pasos) {
-                System.out.println("i=" + i + ", j=" + j);
-            }
-            
-            while (j >= 0 && aux < arreglo[j]) {
-                contComparaciones++;
-                arreglo[j + 1] = arreglo[j];
-                j = j - 1;
-                
+        int contCambios = 0;
+        for (int i = 1; i < n; ++i) {
+            pos = i;
+            aux = arreglo[i];
+            while (pos > 0 && arreglo[pos - 1] > aux) {
                 if (pasos) {
-                    System.out.print("Desplazamiento -> ");
-                    imprimirArreglo(arreglo);
+                    System.out.println("cambiar " + arreglo[pos - 1] + " de la posición " + (pos - 1) + " a la posición " + pos);
                 }
+                arreglo[pos] = arreglo[pos - 1];
+                contComparaciones++;
+                --pos;
+                if (pasos) {
+                    if (pos > 0) {
+                        System.out.println("Comparando " + aux + " posición " + pos + " con " + arreglo[pos - 1] + " posición " + (pos - 1) + "");
+                    } else {
+                        System.out.println("se termina el arreglo");
+                    }
+                }
+                contCambios++;
             }
-            arreglo[j + 1] = aux;
-            
+            arreglo[pos] = aux;
             if (pasos) {
-                System.out.print("Inserción -> ");
+                System.out.println("Colocando " + aux + " en la posición " + pos);
                 imprimirArreglo(arreglo);
             }
         }
-        return contComparaciones;
+        int[] contadores = {contComparaciones, contCambios};
+        return contadores;
     }
-    
-    public int ordenarDes(int[] arreglo, boolean pasos) {
-        int tam = arreglo.length;
+
+    public int[] ordenarDescendente(int[] arreglo, boolean pasos) {
+        int n = arreglo.length;
+        int p = 0;
+        int aux = 0;
         int contComparaciones = 0;
-        
-        for (int i = 1; i < tam; i++) {
-            int aux = arreglo[i];
-            int j = i - 1;
-            
-            if (pasos) {
-                System.out.println("i=" + i + ", j=" + j);
-            }
-            
-            while (j >= 0 && aux > arreglo[j]) {
-                contComparaciones++;
-                arreglo[j + 1] = arreglo[j];
-                j = j - 1;
-                
+        int contCambios = 0;
+        for (int i = 1; i < n; ++i) {
+            p = i;
+            aux = arreglo[i];
+            while (p > 0 && arreglo[p - 1] < aux) {
                 if (pasos) {
-                    System.out.print("Desplazamiento -> ");
-                    imprimirArreglo(arreglo);
+                    System.out.println("Se cumple la condición: " + aux + " > " + arreglo[p - 1]);
+                    System.out.println("Cambiar " + arreglo[p - 1] + " de la posición " + (p - 1) + " a la posición " + p);
                 }
+                arreglo[p] = arreglo[p - 1];
+                contComparaciones++;
+                --p;
+                if (pasos) {
+                    if (p > 0) {
+                        System.out.println("Comparando " + aux + " (posición " + p + ") con " + arreglo[p - 1] + " (posición " + (p - 1) + ")");
+                    } else {
+                        System.out.println("Continuamos con la siguiente posición, se termina el arreglo");
+                    }
+                }
+                contCambios++;
             }
-            arreglo[j + 1] = aux;
-            
+            arreglo[p] = aux;
             if (pasos) {
-                System.out.print("Inserción -> ");
+                System.out.println("Colocando " + aux + " en la posición " + p);
                 imprimirArreglo(arreglo);
             }
         }
-        return contComparaciones;
+        int[] contadores = {contComparaciones, contCambios};
+        return contadores;
     }
-    
-    private void imprimirArreglo(int[] arreglo) {
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.print(arreglo[i] + "-");
+
+    public void imprimirArreglo(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; ++i) {
+            System.out.print(arreglo[i] + " - ");
         }
         System.out.println();
     }
