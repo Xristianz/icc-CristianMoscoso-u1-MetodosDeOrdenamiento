7e0a92a185a6e4b464ccabc0dd30189767ea8b76
Cristian_Moscoso <moscosocristhian130@gmail.com>
2025-03-31 16:14:21 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,125 +1,138 @@
 import java.util.Scanner;
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
-        MetodoBurbuja MetodoBurbujaClase = new MetodoBurbuja();
-        MetodoSeleccion ms = new MetodoSeleccion();
-        MetodoInsercion mi = new MetodoInsercion();
-        BorbujaMejorado mbs = new BorbujaMejorado();
         
+        App main = new App();
+        MetodoBurbuja mB = new MetodoBurbuja();
+        MetodoSeleccion mS = new MetodoSeleccion();
+        MetodoInsercion mI = new MetodoInsercion();
+        BorbujaMejorado mBMejorado = new BorbujaMejorado();
         Scanner scanner = new Scanner(System.in);
         boolean continuar = true;
 
-        while (continuar) {
-            int[] arreglo = new int[10000];
-            for (int i = 0; i < arreglo.length; i++) {
-                arreglo[i] = (int)(Math.random() * 10000) + 1;
-            }
-            
-            System.out.println("Seleccione el metodo de ordenamiento");
-            System.out.println("1 burbuja");
-            System.out.println("2 seleccion");
-            System.out.println("3 insercion");
-            System.out.println("4 burbuja mejorado");
-            System.out.println("5 salir");
-            
-            int metodo = getPositive(scanner, "\t Ingrese la opcion");
-            
-            if (metodo == 5) {
+        while(continuar){
+
+            int[] arreglo = new int[]{12, -7, 25, 0, -15, 33, 19, -22, 5, 48, -3, 27, -30, 14, 7, -1, 41, 20, -11, 8};
+
+            System.out.println("Seleccione el metodo de ordenamiento:");
+            System.out.println("1. Burbuja");
+            System.out.println("2. Seleccion");
+            System.out.println("3. Insersion: ");
+            System.out.println("4. Burbuja Mejorado");
+            System.out.println("5. Terminar");
+            int metodo = scanner.nextInt();
+
+            if(metodo == 5){
+                System.out.println("Fin....Gracias");
                 continuar = false;
                 break;
             }
-            
-            System.out.println("Desea ver los pasos?(true/false)");
-            boolean conPasos = scanner.nextBoolean();
 
-            String[] posibles = {"A", "D"};
-            String orden = getValidString(scanner, posibles, "deseea ordenar asendentemnte(A) o descendente(D)?");
-            boolean ascendente = orden.equalsIgnoreCase("A");
-            int contComparaciones = 0;
-            int[] respuestas;
-            
-            System.out.print("Arreglo original -> ");
-            MetodoBurbujaClase.imprimirArreglo(arreglo);
-            
-            switch(metodo) {
+            int validar=0;
+            String orden= null;
+            while (validar==0){
+                System.out.println("Ingrese de que forma desea ordenar: Asendente (A) o Desendente (D)");
+                orden = scanner.next();
+                if(orden.equalsIgnoreCase("A") || orden.equalsIgnoreCase("D")){
+                    validar = 1;
+                }else{
+                    System.out.println("Seleccione una opcion entre A y D");
+                }
+            }
+
+            System.out.println("Seleccione Con Pasos (True) o Sin Pasos (False)");
+            boolean pasos = scanner.nextBoolean();
+
+            int[] contadores = null;
+            switch (metodo) {
                 case 1:
-                    System.out.println("Metodo de burbuja");
-                    contComparaciones = MetodoBurbujaClase.ordenar(arreglo, !ascendente, conPasos);
-                    System.out.print("Arreglo ordenado -> ");
-                    MetodoBurbujaClase.imprimirArreglo(arreglo);
-                    System.out.println("Comparaciones -> " + contComparaciones);
-                    break;
+                    System.out.println("=====METODO DE BURBUJA====");
+                    System.out.println("El arreglo: ");
+                    main.imprimirArreglo(arreglo);
+                    switch (orden) {
+                        case "A", "a":
+                            contadores = mB.ordenarAscendente(arreglo,pasos);
+                            break;
                     
+                        case "D", "d":
+                            contadores = mB.ordenarDescendente(arreglo, pasos);
+                            break;
+                    }
+                    System.out.println("El arreglo ordenado: ");
+                    main.imprimirArreglo(arreglo);
+                    System.out.println("Comparaciones: "+contadores[0]);
+                    System.out.println("Cambios: "+contadores[1]);
+                    break;
+
                 case 2:
-                    System.out.println("Metodo de seleccion");
-                    if (ascendente) {
-                        contComparaciones = ms.ordenarAsdSeleccion(arreglo, conPasos);
-                    } else {
-                        contComparaciones = ms.ordenarDesSeleccion(arreglo, conPasos);
-                    }
-                    System.out.print("Arreglo ordenado -> ");
-                    MetodoBurbujaClase.imprimirArreglo(arreglo);
-                    System.out.println("Comparaciones -> " + contComparaciones);
-                    break;
+                    System.out.println("====METODO DE SELECCION====");
+                    System.out.println("El arreglo: ");
+                    main.imprimirArreglo(arreglo);
+                    switch (orden) {
+                        case "A", "a":
+                            contadores = mS.ordenarAscendente(arreglo,pasos);
+                            break;
                     
+                        case "D", "d":
+                            contadores = mS.ordenarDescendente(arreglo, pasos);
+                            break;
+                    }
+                    System.out.println("El arreglo ordenado: ");
+                    main.imprimirArreglo(arreglo);
+                    System.out.println("Comparaciones: "+contadores[0]);
+                    System.out.println("Cambios: "+contadores[1]);
+                    break;
+
                 case 3:
-                    System.out.println("Metodo de insercion");
-                    if (ascendente) {
-                        contComparaciones = mi.ordenarAsd(arreglo, conPasos);
-                    } else {
-                        contComparaciones = mi.ordenarDes(arreglo, conPasos);
-                    }
-                    System.out.print("Arreglo ordenado -> ");
-                    MetodoBurbujaClase.imprimirArreglo(arreglo);
-                    System.out.println("Comparaciones -> " + contComparaciones);
-                    break;
+                    System.out.println("====METODO DE INSERCION====");
+                    System.out.println("El arreglo: ");
+                    main.imprimirArreglo(arreglo);
+                    switch (orden) {
+                        case "A", "a":
+                            contadores = mI.ordenarAscendente(arreglo,pasos);
+                            break;
                     
+                        case "D", "d":
+                            contadores = mI.ordenarDescendente(arreglo, pasos);
+                            break;
+                    }
+                    System.out.println("El arreglo ordenado: ");
+                    main.imprimirArreglo(arreglo);
+                    System.out.println("Comparaciones: "+contadores[0]);
+                    System.out.println("Cambios: "+contadores[1]);
+                    break;
+
                 case 4:
-                    System.out.println("Metodo de burbuja mejorado");
-                    if (ascendente) {
-                        respuestas = mbs.ordenarAscendente(arreglo, conPasos);
-                    } else {
-                        respuestas = mbs.ordenarDescendente(arreglo, conPasos);
-                    }
-                    System.out.print("Arreglo ordenado -> ");
-                    mbs.imprimirArreglo(arreglo);
-                    System.out.println("Comparaciones -> " + respuestas[0]);
-                    System.out.println("Intercambios -> " + respuestas[1]);
-                    break;
+                    System.out.println("====METODO DE BURBUJA MEJORADO====");
+                    System.out.println("El arreglo: ");
+                    main.imprimirArreglo(arreglo);
+                    switch (orden) {
+                        case "A", "a":
+                            contadores = mBMejorado.ordenarAscendente(arreglo,pasos);
+                            break;
                     
+                        case "D", "d":
+                            contadores = mBMejorado.ordenarDescendente(arreglo, pasos);
+                            break;
+                    }
+                    System.out.println("El arreglo ordenado es: ");
+                    main.imprimirArreglo(arreglo);
+                    System.out.println("Comparaciones: "+contadores[0]);
+                    System.out.println("Cambios: "+contadores[1]);
+                    break;
                 default:
-                    System.out.println("Opcion incorrecta");
+                    System.out.println("Opcion Incorrecta");
+                    break;
             }
         }
     }
-
-    public static int getPositive(Scanner scanner, String message) {
-        int number;
-        do {
-            System.out.print(message + ": ");
-            number = scanner.nextInt();
-            if (number <= 0) {
-                System.out.println("El numero debe ser positivo. Intente nuevamente");
-            }
-        } while (number <= 0);
-        return number;
-    }
-
-    public static String getValidString(Scanner scanner, String[] posibles, String message) {
-        String input;
-        boolean valido;
-        do {
-            System.out.print(message + ": ");
-            input = scanner.next();
-            valido = false;
-            for (String posible : posibles) {
-                if (input.equalsIgnoreCase(posible)) {
-                    valido = true;
-                    break;
-                }
-            }
-        } while (!valido);
-        return input;
+    public void imprimirArreglo(int[] arreglo) {
+        for(int i = 0; i < arreglo.length; ++i) {
+            System.out.print(arreglo[i] + " - ");
+        }
+        System.out.println();
     }
 }
+
